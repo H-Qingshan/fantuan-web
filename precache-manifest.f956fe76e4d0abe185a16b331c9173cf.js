f3e9762518379058deaf9d1d9365719c151f81c7
Hill <1292331340@qq.com>
2022-05-10 21:21:43 +0800

    deploy

---
+++ precache-manifest.f956fe76e4d0abe185a16b331c9173cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571b59171e6a07823111",
+    "url": "/fantuan-web/css/app.ba464ba5.css"
+  },
+  {
+    "revision": "8c03303cf367d91e6d346af20e6fcd5b",
+    "url": "/fantuan-web/index.html"
+  },
+  {
+    "revision": "571b59171e6a07823111",
+    "url": "/fantuan-web/js/app.dfc100ec.js"
+  },
+  {
+    "revision": "d8c7ed4e8abae4c00f52",
+    "url": "/fantuan-web/js/chunk-vendors.770dc180.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/fantuan-web/manifest.json"
+  },
+  {
+    "revision": "2f083f32b1264c1fb3f6031e81891af8",
+    "url": "/fantuan-web/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fantuan-web/robots.txt"
+  }
+]);
